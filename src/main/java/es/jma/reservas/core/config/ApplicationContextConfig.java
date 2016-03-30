9439df4d638ac04a8ffca81cf13e0afae4625916
Jorge <j.tulon@gmail.com>
2016-03-30 08:06:44 +0100

    Actualizado fichero de configuración

--- src/main/java/es/jma/reservas/core/config/ApplicationContextConfig.java
+++ src/main/java/es/jma/reservas/core/config/ApplicationContextConfig.java
@@ -48,9 +48,9 @@ public class ApplicationContextConfig {
     public DataSource getDataSource() {
     	BasicDataSource dataSource = new BasicDataSource();
     	dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-    	dataSource.setUrl("jdbc:mysql://vps81773.ovh.net:3306/SistemaReservas");
-    	dataSource.setUsername("root");
-    	dataSource.setPassword("bsg-075-William-Adama");
+    	dataSource.setUrl("jdbc:mysql://url:3306/SistemaReservas");
+    	dataSource.setUsername("username");
+    	dataSource.setPassword("secret");
     	
     	return dataSource;
     }
